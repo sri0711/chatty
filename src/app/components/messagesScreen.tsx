9f69b57816012d8cc2b847fad4ace78b7482483d
sri0711 <m.sathish0711@gmail.com>
2024-12-19 22:37:18 +0530

    ui finished

--- src/app/components/messagesScreen.tsx
+++ src/app/components/messagesScreen.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef } from "react";
-import { View, Text, FlatList } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { View, Text, FlatList, Animated } from "react-native";
 import { useSelector } from "react-redux";
 import { State } from "@/src/constants/interfaces";
 import messageStyle from "@/src/app/styles/messageStyle";
+import { PanGestureHandler } from "react-native-gesture-handler";
 
 const MessagesScreen = () => {
   const flatListRef = useRef<FlatList | null>(null);
@@ -17,21 +18,73 @@ const MessagesScreen = () => {
     }, 500);
     return () => clearTimeout(messageDelay);
   }, [messages]);
+
+  // for swipe action
+  const translateXValues = useRef<{ [key: number]: Animated.Value }>({});
+
+  const onGestureEvent = (index: number) => {
+    return Animated.event(
+      [{ nativeEvent: { translationX: translateXValues.current[index] } }],
+      {
+        useNativeDriver: true,
+      }
+    );
+  };
+
+  const actionForReply = (event: any, index: number, data: any) => {
+    const { translationX, state } = event.nativeEvent;
+
+    if (state === 5) {
+      // Gesture ended
+      if (translationX > 100) {
+        console.log("Replying to message:", data);
+      }
+
+      // Reset position after swipe action
+      Animated.spring(translateXValues.current[index], {
+        toValue: 0, // Reset to initial position
+        useNativeDriver: true,
+      }).start();
+    }
+  };
   return (
     <View>
       <FlatList
         ref={flatListRef}
         style={messageStyle.flatList}
         data={messages}
-        renderItem={({ item }) => (
-          <View>
-            <Text style={messageStyle.messageText}>
-              <Text style={messageStyle.userText}>{item.name}@chatty$ </Text>
-              {"  "}
-              {item.message}
-            </Text>
-          </View>
-        )}
+        renderItem={({ item, index }) => {
+          if (!translateXValues.current[index]) {
+            translateXValues.current[index] = new Animated.Value(0);
+          }
+
+          return (
+            <View>
+              <PanGestureHandler
+                onGestureEvent={onGestureEvent(index)}
+                onHandlerStateChange={(event) =>
+                  actionForReply(event, index, item)
+                }
+              >
+                <Animated.View
+                  style={{
+                    transform: [
+                      { translateX: translateXValues.current[index] },
+                    ],
+                  }}
+                >
+                  <Text style={messageStyle.messageText}>
+                    <Text style={messageStyle.userText}>
+                      {item.name}@chatty${" "}
+                    </Text>
+                    {"  "}
+                    {item.message}
+                  </Text>
+                </Animated.View>
+              </PanGestureHandler>
+            </View>
+          );
+        }}
       />
     </View>
   );
